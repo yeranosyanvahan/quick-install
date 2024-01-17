726820733b2805dc1c145f19f7eebeebc45d4e7e
Vahan Yeranosyan <yeranosyanvahan@gmail.com>
2024-01-17 21:36:38 +0400

    use v1.26.12+k3s1

--- k3s/server.sh
+++ k3s/server.sh
@@ -1,4 +1,4 @@
-INSTALL_K3S_VERSION=v1.28.5+k3s1
+INSTALL_K3S_VERSION=v1.26.12+k3s1
 curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=${INSTALL_K3S_VERSION} sh -s - server --cluster-cidr=192.168.0.0/16
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 bash ./get_helm.sh
