f91b0a2315deefef1c74c91ec7fdf48f1bdeb89d
yeranosyanvahan <yeranosyanvahan@gmail.com>
2024-03-08 20:51:30 +0400

    update env and nodeport installation

--- k3s/rancher-install-nodeport.sh
+++ k3s/rancher-install-nodeport.sh
@@ -11,6 +11,14 @@ helm repo add rancher-stable https://releases.rancher.com/server-charts/stable
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
 
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/${CERT_MANAGER_VERSION}/cert-manager.crds.yaml
+
+helm ${HELMCOMMAND} cert-manager jetstack/cert-manager \
+  --namespace cert-manager \
+  --create-namespace \
+  --version ${CERT_MANAGER_VERSION}
+
+
 kubectl create namespace cattle-system
 
 helm ${HELMCOMMAND} rancher rancher-stable/rancher \
